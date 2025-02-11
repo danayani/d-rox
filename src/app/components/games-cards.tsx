22fc985c29291942e6df2019115a936666d4f940
dana-yaniv <dana.yaniv@ornelix.com>
2025-02-11 22:46:50 +0200

    fetch inside 'games-cards' cmp (client cmp) using useState and useEffect

--- src/app/components/games-cards.tsx
+++ src/app/components/games-cards.tsx
@@ -1,14 +1,30 @@
 "use client"
 import {Game} from "@/app/types";
+import {useEffect, useState} from "react";
 
 
 
-export default function GamesCards(props:{games:Game[]}) {
+export default function GamesCards() {
+    const [gamesShows, setGamesShows] = useState<Game[]>([])
+    const [isLoading, setLoading] = useState(true)
+    console.log(gamesShows)
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/games`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("data = ", data)
+                setGamesShows([...gamesShows, ...data])
+                setLoading(false)
+            })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
     return <div className={`w-full justify-center flex flex-row flex-wrap gap-4 pt-6`}
      >
-        {props.games.map((item) => (
-            <div key={item.id}>
-                <Card game={item}/>
+        {gamesShows.map((game) => (
+            <div key={game.id}>
+                <Card game={game}/>
             </div>
         ))}
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import GamesCards from "@/app/components/games-cards";
 
 export default async function Home() {
-    const data = await fetch(`http://localhost:3000/api/games`)
-    const allGames = await data.json()
-    console.log(allGames)
 
-  if(!allGames) return <div>Loading...</div>
   return (
     <div>
-      <GamesCards games={allGames}/>
+      <GamesCards/>
     </div>
   )
 }
