5f19aaac42f099244785dfad6eeeb7eafc5b414b
dana-yaniv <dana.yaniv@ornelix.com>
2025-02-12 17:40:40 +0200

    implement tailwind color, and change 'body' bg and font-family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        Purple: "#8860FF",
+        PurpleBlue:"#6A38FF",
+        Blue: "#122c75",
+        DarkBlue: "#0A1F59",
+        LightGreen: "#B7F763",
       },
     },
   },
